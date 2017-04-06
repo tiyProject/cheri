754619992b3a7d3a88d45e56da2705c0488d0e51
Rob <epr1935@icloud.com>
2017-04-06 16:23:00 -0400

    added a user_notifier_mailer, and added UserNotifier.send_signup_email to the as yet unused order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,12 @@
 class OrdersController < ApplicationController
+
+  def new
+  end
+
+  def create
+    #after this is filled in, after the .save
+    #include the following line
+    # UserNotifier.send_signup_email(@order).deliver
+  end
+
 end
