c88d729b7fa379fd1cff95de866283e1c2adf842
kelsey vaught <kelsey.vaught92@gmail.com>
2017-04-20 10:46:52 -0400

    building

--- app-react/src/pages/Checkout.js
+++ app-react/src/pages/Checkout.js
@@ -1,122 +1,120 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-class Checkout extends Component {
-  render() {
-    return (
-      <Iam />
-      <div className="container">
-        <div className="columns">
-          <div className="column is-half">
-            <div className="frameRed">
-              <div className="orderImage">
-                <img src={this.props.sizes[0].photo} alt={this.props.sizes[0].title} />
-              </div>
-            </div>
-          </div>
-          <div className="column">
-            <div className="column is-half">
-              <div className="frameRed">
-                <div className="order">
-                  <header className="card-header">
-                    <p className="card-header-title">
-                      {this.props.title}
-                    </p>
-                  </header>
-                  <div className="field">
-                    <label className="label">Name</label>
-                    <p className="control">
-                      <input className="input" type="text" placeholder="Text input" />
-                    </p>
-                  </div>
+// class Checkout extends Component {
+//   render() {
+//     return <Iam />
+//       <div className="container">
+//         <div className="columns">
+//           <div className="column is-half">
+//             <div className="frameRed">
+//               <div className="orderImage">
+//                 <img src={this.props.sizes[0].photo} alt={this.props.sizes[0].title} />
+//               </div>
+//             </div>
+//           </div>
+//             <div className="column is-half">
+//               <div className="frameRed">
+//                 <div className="order">
+//                   <header className="card-header">
+//                     <p className="card-header-title">
+//                       {this.props.title}
+//                     </p>
+//                   </header>
+//                   <div className="field">
+//                     <label className="label">Name</label>
+//                     <p className="control">
+//                       <input className="input" type="text" placeholder="Text input" />
+//                     </p>
+//                   </div>
 
-                  <div className="field">
-                    <label className="label">Username</label>
-                    <p className="control has-icons-left has-icons-right">
-                      <input className="input is-success" type="text" placeholder="Text input" value="bulma" />
-                      <span className="icon is-small is-left">
-                        <i className="fa fa-user"></i>
-                      </span>
-                      <span className="icon is-small is-right">
-                        <i className="fa fa-check"></i>
-                      </span>
-                    </p>
-                    <p className="help is-success">This username is available</p>
-                  </div>
+//                   <div className="field">
+//                     <label className="label">Username</label>
+//                     <p className="control has-icons-left has-icons-right">
+//                       <input className="input is-success" type="text" placeholder="Text input" value="bulma" />
+//                       <span className="icon is-small is-left">
+//                         <i className="fa fa-user"></i>
+//                       </span>
+//                       <span className="icon is-small is-right">
+//                         <i className="fa fa-check"></i>
+//                       </span>
+//                     </p>
+//                     <p className="help is-success">This username is available</p>
+//                   </div>
 
-                  <div className="field">
-                    <label className="label">Email</label>
-                    <p className="control has-icons-left has-icons-right">
-                      <input className="input is-danger" type="text" placeholder="Email input" value="hello@" />
-                      <span className="icon is-small is-left">
-                        <i className="fa fa-envelope"></i>
-                      </span>
-                      <span className="icon is-small is-right">
-                        <i className="fa fa-warning"></i>
-                      </span>
-                    </p>
-                    <p className="help is-danger">This email is invalid</p>
-                  </div>
+//                   <div className="field">
+//                     <label className="label">Email</label>
+//                     <p className="control has-icons-left has-icons-right">
+//                       <input className="input is-danger" type="text" placeholder="Email input" value="hello@" />
+//                       <span className="icon is-small is-left">
+//                         <i className="fa fa-envelope"></i>
+//                       </span>
+//                       <span className="icon is-small is-right">
+//                         <i className="fa fa-warning"></i>
+//                       </span>
+//                     </p>
+//                     <p className="help is-danger">This email is invalid</p>
+//                   </div>
 
-                  <div className="field">
-                    <label className="label">Subject</label>
-                    <p className="control">
-                      <span className="select">
-                        <select>
-                          <option>Select dropdown</option>
-                          <option>With options</option>
-                        </select>
-                      </span>
-                    </p>
-                  </div>
+//                   <div className="field">
+//                     <label className="label">Subject</label>
+//                     <p className="control">
+//                       <span className="select">
+//                         <select>
+//                           <option>Select dropdown</option>
+//                           <option>With options</option>
+//                         </select>
+//                       </span>
+//                     </p>
+//                   </div>
 
-                  <div className="field">
-                    <label className="label">Message</label>
-                    <p className="control">
-                      <textarea className="textarea" placeholder="Textarea"></textarea>
-                    </p>
-                  </div>
+//                   <div className="field">
+//                     <label className="label">Message</label>
+//                     <p className="control">
+//                       <textarea className="textarea" placeholder="Textarea"></textarea>
+//                     </p>
+//                   </div>
 
-                  <div className="field">
-                    <p className="control">
-                      <label className="checkbox">
-                        <input type="checkbox" />
-                        I agree to the <a href="#">terms and conditions</a>
-                      </label>
-                    </p>
-                  </div>
+//                   <div className="field">
+//                     <p className="control">
+//                       <label className="checkbox">
+//                         <input type="checkbox" />
+//                         I agree to the <a href="#">terms and conditions</a>
+//                       </label>
+//                     </p>
+//                   </div>
 
-                  <div className="field">
-                    <p className="control">
-                      <label className="radio">
-                        <input type="radio" name="question" />
-                        Yes
-    </label>
-                      <label className="radio">
-                        <input type="radio" name="question" />
-                        No
-    </label>
-                    </p>
-                  </div>
+//                   <div className="field">
+//                     <p className="control">
+//                       <label className="radio">
+//                         <input type="radio" name="question" />
+//                         Yes
+//     </label>
+//                       <label className="radio">
+//                         <input type="radio" name="question" />
+//                         No
+//     </label>
+//                     </p>
+//                   </div>
 
-                  <div className="field is-grouped">
-                    <p className="control">
-                      <button className="button is-primary">Submit</button>
-                    </p>
-                    <p className="control">
-                      <button className="button is-link">Cancel</button>
-                    </p>
-                  </div>
+//                   <div className="field is-grouped">
+//                     <p className="control">
+//                       <button className="button is-primary">Submit</button>
+//                     </p>
+//                     <p className="control">
+//                       <button className="button is-link">Cancel</button>
+//                     </p>
+//                   </div>
 
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      
-    );
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//           </div>
+       
+   
     
-  }
-}
+    
+//   }
+// }
 
-export default Checkout;
+// export default Checkout;
